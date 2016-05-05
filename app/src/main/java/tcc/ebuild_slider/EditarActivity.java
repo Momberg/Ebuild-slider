103e92c39b460d9cb8f7c5ab55971ee4c9a638f0
Gabriel Momberg <gabriel.momberg@cognizant.com>
2016-05-04 21:09:38 -0300

    Java e interface:
    - Implementação de fase de obra anterior no formulário de edição.
    
    - Alteração das interfaces dos formulários de criação e edição de obras.

--- app/src/main/java/tcc/ebuild_slider/EditarActivity.java
+++ app/src/main/java/tcc/ebuild_slider/EditarActivity.java
@@ -5,21 +5,24 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 public class EditarActivity extends AppCompatActivity {
 
-    private static final int INTERNO = 2131558558;
-    private static final int EXTERNO = 2131558559;
+    private static final int INTERNO = 2131558560;
+    private static final int EXTERNO = 2131558561;
     private static final String[] interno =  new String[] {"Verificar a necessidade","Elaboração dos estudos técnicos preliminares","Licença ambiental prévia","Elaboração do projeto básico","Elaboração do projeto executivo"};
     private static final String[] externo = new String[] {"Publicação do edital","Licitação","Contrataçao e designação do fiscal da obra","Pagamento seguindo o cronograma físico-financeiro e ordem cronológica","Recebimento da obra","Devolução de garantia","Registros finais"};
     EditText nome, data, rua, bairro, cidade;
@@ -27,6 +30,7 @@ public class EditarActivity extends AppCompatActivity {
     String nome_obra, data_obra, rua_obra, bairro_obra, cidade_obra;
     RadioButton int_ext, in, out;
     RadioGroup GrupoRadio;
+    TextView fase_ant;
     String item_selecionado, temp, item_da_base;
     ListView lista;
     int item = 0, ID_obra;
@@ -47,6 +51,105 @@ public class EditarActivity extends AppCompatActivity {
         find_IDs();
         getID_para_banco();
         fases();
+        botoes();
+    }
+
+    private void armazenar_info(){
+        nome_obra = nome.getText().toString();
+        data_obra = data.getText().toString();
+        rua_obra = rua.getText().toString();
+        bairro_obra = bairro.getText().toString();
+        cidade_obra = cidade.getText().toString();
+        temp = int_ext.getText().toString();
+    }
+
+    public void salvar(){
+        o.edit_obra(nome_obra, data_obra, rua_obra, bairro_obra, cidade_obra, temp, item_selecionado);
+        db.save(o);
+    }
+
+    public void fases(){
+        GrupoRadio = (RadioGroup) findViewById(R.id.radio_Ext_Int);
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                item_selecionado = (String) arg0.getAdapter().getItem(arg2);
+                item = 1;
+            }
+        });
+        int_ext = (RadioButton) findViewById(GrupoRadio.getCheckedRadioButtonId());
+        GrupoRadio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                int selectedId = GrupoRadio.getCheckedRadioButtonId();
+                int_ext = (RadioButton) findViewById(selectedId);
+                if ((int_ext = (RadioButton) findViewById(selectedId)) != null) {
+                    if (selectedId == INTERNO) {
+                        adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, interno);
+                        lista.setAdapter(adapter);
+                    }
+                    if (selectedId == EXTERNO) {
+                        adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, externo);
+                        lista.setAdapter(adapter);
+                    }
+                }
+            }
+        });
+    }
+
+    private void toast(String text){
+        Toast.makeText(getBaseContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    private void find_IDs(){
+        nome = (EditText) findViewById(R.id.nome);
+        data = (EditText) findViewById(R.id.data);
+        rua = (EditText) findViewById(R.id.rua);
+        bairro = (EditText) findViewById(R.id.bairro);
+        cidade = (EditText) findViewById(R.id.cidade);
+        in = (RadioButton) findViewById(R.id.radioInterno);
+        out = (RadioButton) findViewById(R.id.radioExterno);
+        lista = (ListView) findViewById(R.id.list_Int_Ext);
+        fase_ant = (TextView) findViewById(R.id.text_fase_anterior);
+    }
+
+    private void getID_para_banco(){
+        ID_obra = cod_obra.getInt("ID", 0);
+        obra = service.getObrabyID(getApplicationContext(), ID_obra);
+        o = obra.get(0);
+        nome.setText(o.getNome());
+        data.setText(o.getData());
+        rua.setText(o.getRua());
+        bairro.setText(o.getBairro());
+        cidade.setText(o.getCidade());
+        item_da_base = o.getFase();
+        if(o.getTipoFase().equals("Interna")){
+            in.setChecked(true);
+            adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, interno);
+            lista.setAdapter(adapter);
+            int i = 0;
+            for(String inte : interno){
+                inte = interno[i];
+                if(inte.equals(item_da_base)){
+                    fase_ant.setText(item_da_base);
+                }
+                i += 1;
+            }
+        } else {
+            out.setChecked(true);
+            adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, externo);
+            lista.setAdapter(adapter);
+            int i = 0;
+            for(String exte : externo){
+                exte = externo[i];
+                if(exte.equals(item_da_base)){
+                    fase_ant.setText(item_da_base);
+                }
+                i += 1;
+            }
+        }
+    }
+
+    private void botoes(){
         salvar = (Button) findViewById(R.id.Bsalvar);
         salvar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,102 +173,5 @@ public class EditarActivity extends AppCompatActivity {
                 finish();
             }
         });
-    }
-
-    private void armazenar_info(){
-        nome_obra = nome.getText().toString();
-        data_obra = data.getText().toString();
-        rua_obra = rua.getText().toString();
-        bairro_obra = bairro.getText().toString();
-        cidade_obra = cidade.getText().toString();
-        temp = int_ext.getText().toString();
-    }
-
-    public void salvar(){
-        o.edit_obra(nome_obra, data_obra, rua_obra, bairro_obra, cidade_obra, temp, item_selecionado);
-        db.save(o);
-    }
-
-    public void fases(){
-        GrupoRadio = (RadioGroup) findViewById(R.id.radio_Ext_Int);
-        lista.setAdapter(adapter);
-        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                item_selecionado = (String) arg0.getAdapter().getItem(arg2);
-                item = 1;
-                toast("index selecionado: " + arg2);
-            }
-        });
-        int_ext = (RadioButton) findViewById(GrupoRadio.getCheckedRadioButtonId());
-        GrupoRadio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                int selectedId = GrupoRadio.getCheckedRadioButtonId();
-                int_ext = (RadioButton) findViewById(selectedId);
-                if ((int_ext = (RadioButton) findViewById(selectedId)) != null) {
-                    if (selectedId == INTERNO) {
-                        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, interno);
-                        lista = (ListView) findViewById(R.id.list_Int_Ext);
-                        lista.setAdapter(adapter);
-                    }
-                    if (selectedId == EXTERNO) {
-                        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, externo);
-                        lista = (ListView) findViewById(R.id.list_Int_Ext);
-                        lista.setAdapter(adapter);
-                    }
-                    lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                            item_selecionado = (String) arg0.getAdapter().getItem(arg2);
-                            item = 1;
-                        }
-                    });
-                }
-            }
-        });
-    }
-
-    private void toast(String text){
-        Toast.makeText(getBaseContext(), text, Toast.LENGTH_SHORT).show();
-    }
-
-    private void find_IDs(){
-        nome = (EditText) findViewById(R.id.nome);
-        data = (EditText) findViewById(R.id.data);
-        rua = (EditText) findViewById(R.id.rua);
-        bairro = (EditText) findViewById(R.id.bairro);
-        cidade = (EditText) findViewById(R.id.cidade);
-        in = (RadioButton) findViewById(R.id.radioInterno);
-        out = (RadioButton) findViewById(R.id.radioExterno);
-    }
-
-    private void getID_para_banco(){
-        ID_obra = cod_obra.getInt("ID", 0);
-        obra = service.getObrabyID(getApplicationContext(), ID_obra);
-        lista = (ListView) findViewById(R.id.list_Int_Ext);
-        o = obra.get(0);
-        nome.setText(o.getNome());
-        data.setText(o.getData());
-        rua.setText(o.getRua());
-        bairro.setText(o.getBairro());
-        cidade.setText(o.getCidade());
-        item_da_base = o.getFase();
-        if(o.getTipoFase().equals("Interna")){
-            in.setChecked(true);
-            adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, interno);
-            int i = 0;
-            for(String inte : interno){
-                inte = interno[i];
-                if(inte.equals(item_da_base)){
-                    lista.setItemChecked(i, true);
-                    lista.setSelection(i);
-                    lista.setSelected(true);
-                    lista.getOnItemSelectedListener();
-                }
-                i += 1;
-            }
-        } else {
-            out.setChecked(true);
-            adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, externo);
-        }
     }
 }
--- app/src/main/java/tcc/ebuild_slider/FormActivity.java
+++ app/src/main/java/tcc/ebuild_slider/FormActivity.java
@@ -16,8 +16,8 @@ import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
-    private static final int INTERNO = 2131558558;
-    private static final int EXTERNO = 2131558559;
+    private static final int INTERNO = 2131558560;
+    private static final int EXTERNO = 2131558561;
     private static final String[] interno =  new String[] {"Verificar a necessidade","Elaboração dos estudos técnicos preliminares","Licença ambiental prévia","Elaboração do projeto básico","Elaboração do projeto executivo"};
     private static final String[] externo = new String[] {"Publicação do edital","Licitação","Contrataçao e designação do fiscal da obra","Pagamento seguindo o cronograma físico-financeiro e ordem cronológica","Recebimento da obra","Devolução de garantia","Registros finais"};
     EditText nome, data, rua, bairro, cidade;
@@ -35,18 +35,68 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
-        find_IDs();
-        fases();
         preenchido = false;
         cod_final = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        if(!cod_final.getString("nome", "").equals("")){
-            nome.setText(cod_final.getString("nome", ""));
-            data.setText(cod_final.getString("data", ""));
-            rua.setText(cod_final.getString("rua", ""));
-            bairro.setText(cod_final.getString("bairro", ""));
-            cidade.setText(cod_final.getString("cidade", ""));
-        }
+        find_IDs();
+        fases();
+        botoes();
+        get_info_if_exist();
+    }
 
+    public void salvar(){
+        nome_obra = nome.getText().toString();
+        data_obra = data.getText().toString();
+        rua_obra = rua.getText().toString();
+        bairro_obra = bairro.getText().toString();
+        cidade_obra = cidade.getText().toString();
+    }
+
+    public void fases(){
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, interno);
+        lista.setAdapter(adapter);
+        int temId = GrupoRadio.getCheckedRadioButtonId();
+        int_ext = (RadioButton) findViewById(temId);
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                item_selecionado = (String) arg0.getAdapter().getItem(arg2);
+                item = 1;
+            }
+        });
+
+        GrupoRadio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                int selectedId = GrupoRadio.getCheckedRadioButtonId();
+                int_ext = (RadioButton) findViewById(selectedId);
+                if ((int_ext = (RadioButton) findViewById(selectedId)) != null) {
+                    if (selectedId == INTERNO) {
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, interno);
+                        lista.setAdapter(adapter);
+                    }
+                    if (selectedId == EXTERNO) {
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, externo);
+                        lista.setAdapter(adapter);
+                    }
+                }
+            }
+        });
+    }
+
+    private void toast(String text){
+        Toast.makeText(getBaseContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    private void find_IDs(){
+        nome = (EditText) findViewById(R.id.nome);
+        data = (EditText) findViewById(R.id.data);
+        rua = (EditText) findViewById(R.id.rua);
+        bairro = (EditText) findViewById(R.id.bairro);
+        cidade = (EditText) findViewById(R.id.cidade);
+        lista = (ListView) findViewById(R.id.list_Int_Ext);
+        GrupoRadio = (RadioGroup) findViewById(R.id.radio_Ext_Int);
+    }
+
+    private void botoes(){
         salvar = (Button) findViewById(R.id.Bsalvar);
         salvar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,65 +130,13 @@ public class FormActivity extends AppCompatActivity {
         });
     }
 
-    public void salvar(){
-        nome_obra = nome.getText().toString();
-        data_obra = data.getText().toString();
-        rua_obra = rua.getText().toString();
-        bairro_obra = bairro.getText().toString();
-        cidade_obra = cidade.getText().toString();
+    private void get_info_if_exist(){
+        if(!cod_final.getString("nome", "").equals("")){
+            nome.setText(cod_final.getString("nome", ""));
+            data.setText(cod_final.getString("data", ""));
+            rua.setText(cod_final.getString("rua", ""));
+            bairro.setText(cod_final.getString("bairro", ""));
+            cidade.setText(cod_final.getString("cidade", ""));
+        }
     }
-
-    public void fases(){
-        GrupoRadio = (RadioGroup) findViewById(R.id.radio_Ext_Int);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, interno);
-        lista = (ListView) findViewById(R.id.list_Int_Ext);
-        lista.setAdapter(adapter);
-        int temId = GrupoRadio.getCheckedRadioButtonId();
-        int_ext = (RadioButton) findViewById(temId);
-        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                item_selecionado = (String) arg0.getAdapter().getItem(arg2);
-                item = 1;
-            }
-        });
-
-        GrupoRadio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                int selectedId = GrupoRadio.getCheckedRadioButtonId();
-                int_ext = (RadioButton) findViewById(selectedId);
-                if ((int_ext = (RadioButton) findViewById(selectedId)) != null) {
-                    if (selectedId == INTERNO) {
-                        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, interno);
-                        lista = (ListView) findViewById(R.id.list_Int_Ext);
-                        lista.setAdapter(adapter);
-                    }
-                    if (selectedId == EXTERNO) {
-                        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, externo);
-                        lista = (ListView) findViewById(R.id.list_Int_Ext);
-                        lista.setAdapter(adapter);
-                    }
-                    lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                            item_selecionado = (String) arg0.getAdapter().getItem(arg2);
-                            item = 1;
-                        }
-                    });
-                }
-            }
-        });
-    }
-
-    private void toast(String text){
-        Toast.makeText(getBaseContext(), text, Toast.LENGTH_SHORT).show();
-    }
-
-    private void find_IDs(){
-        nome = (EditText) findViewById(R.id.nome);
-        data = (EditText) findViewById(R.id.data);
-        rua = (EditText) findViewById(R.id.rua);
-        bairro = (EditText) findViewById(R.id.bairro);
-        cidade = (EditText) findViewById(R.id.cidade);
-    }
-
 }
